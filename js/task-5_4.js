bc9a0a5987af53d01d8c98b1adf0454396592388
Pavel Sakun <axelhomkin@gmail.com>
2020-12-24 12:41:50 +0200

    task one

---
+++ js/task-5_4.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const countTotalSalary = function (employees) {
+  let totalSalary = 0;
+  for (let key in employees) totalSalary += employees[key];
+  return totalSalary;
+  // const values = Object.values(employees);
+
+  // for (const value of values) {
+  //   totalSalary += value;
+  // }
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
---
+++ js/task-5_5.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const unknown = [];
+  for (const friend of arr) if (friend[prop]) unknown.push(friend[prop]);
+  return unknown;
+};
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// const getAllPropValues = function (arr, prop) {
+//   const names = [];
+//   for (const index of arr) {
+//     // console.log(index);
+//     names.push(arr.prop);
+//     // console.log(names);
+//   }
+//   return names;
+// };
+
+// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+// console.log(getAllPropValues(products, 'category')); // []
+
+// 5) console.log(getAllPropValues(products, 'category')); // []
+
+// получаешь
+// [undefined, undefined, undefined, undefined]
+
+// Тебе нужно делать проверку прежде чем пушить
+// friend[prop] ?unknown.push(friend[prop])
+// : ""
